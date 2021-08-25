e8a5a6d74ab67970e43141c137a14c18b878e24c
achiocch <andreas.chiocchetti@web.de>
2021-08-25 13:16:27 +0200

    updates Preprocessing

--- setup_start.R
+++ setup_start.R
@@ -1,12 +1,10 @@
 library(workflowr)
 
 
-setwd("S:/KJP_Biolabor/Projekte/RAISE_GENIC/")
+setwd("S:/KJP_Biolabor/00_KJP_Biolabor/Projects/RAISE_GENIC/")
 wflow_start("./", existing = TRUE, git=T)
 
-wflow_use_github(organization = "KJPMolgenLab", 
+wflow_use_github(organization = "KJPMolgenLab",
                  repository = "RAISE_genic")
 
-
 source("code/00_Installer.R")
-
