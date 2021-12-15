2714bf477696a3660ef55150316b95ee705688df
achiocch <andreas.chiocchetti@web.de>
2021-12-15 14:05:24 +0100

    adds network feature calculations

--- setup_start.R
+++ setup_start.R
@@ -2,8 +2,8 @@ library(workflowr)
 
 
 setwd("S:/KJP_Biolabor/00_KJP_Biolabor/Projects/RAISE_GENIC/")
-wflow_start("./", existing = TRUE, git=T)
 
+wflow_start("./", existing = TRUE, git=T)
 wflow_use_github(organization = "KJPMolgenLab",
                  repository = "RAISE_genic")
 
@@ -14,7 +14,7 @@ wflow_build("analysis/index.Rmd")
 system.time(wflow_publish(c("analysis/*","docs/*","code/*")))
 system("git push -u origin master")
 
-
 system("git add docs/* analysis/* code/*")
-system("git commit -a -m \"adds statistical analysis\"")
+system("git commit -a -m \"adds index analysis\"")
 system("git push -u origin master")
+
